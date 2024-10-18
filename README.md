cf709ef734e1f14df178ba5b83cbb2796b853494
Elan Medoff <54599408+ElanMedoff@users.noreply.github.com>
2024-10-18 00:55:35 -0400

    update flag parsing

--- README.md
+++ README.md
@@ -2,20 +2,20 @@
 
 ### work in progress
 
-A bash script to track move back and forth along directories you've visited - easily navigating each step in your
-history ... like a time machine.
+A bash script to move back and forth along the directories you've visited. easily navigate each step in your history ...
+like you have a time machine.
 
 ---
 
 ```bash
 $ pwd # /one
-$ source path/to/cd_time_machine/main.sh two
+$ source path/to/cd_time_machine/main.sh --script_dir=path/to/cd_time_machine --change_dir=two
 $ pwd # /one/two`
-$ source path/to/cd_time_machine/main.sh three
+$ source path/to/cd_time_machine/main.sh --script_dir=path/to/cd_time_machine --change_dir=three
 $ pwd # /one/two/three`
-$ source path/to/cd_time_machine/main.sh --backwards
+$ source path/to/cd_time_machine/main.sh --script_dir=path/to/cd_time_machine --backwards
 $ pwd # /one/two
-$ source path/to/cd_time_machine/main.sh --forwards
+$ source path/to/cd_time_machine/main.sh --script_dir=path/to/cd_time_machine --forwards
 $ pwd # /one/two/three
 ```
 
@@ -24,33 +24,34 @@ $ pwd # /one/two/three
 ### Q: Why do you need to execute the script with `source`, why not with `./`?
 
 A: When executing a script with `./`, it's spawned in a sub-shell. Changing your directory in the subshell only applies
-_within_ the subshell, and it won't affect the shell where you initially typed `./`. In order to run a script within
-your current shell - and have directory changes take affect in your current shell - the `source` keyword is necessary.
+_within_ the subshell, and it won't affect the shell where you initially executed `cd_time_machine`. In order to run a
+script within your current shell - and have directory changes take affect in your current shell - the `source` keyword
+is necessary.
 
 ### Q: Why Deno?
 
 ### Q: Is `cd_time_machine` fast?
 
-A: very! About `0.005s` for a directory change or backwards navigation, `0.03s` for a forwards navigation. To keep
-things speedy, I keep calls to Deno _after_ any navigation, and send the job to the background to avoid blocking the
-main script. The exception is a forwards navigation, since that requires reading from the database before navigating.
+A: very! About `0.005s` for a directory change or backwards navigation, `0.03s` for a forwards navigation. To make
+things fast, I keep calls to Deno _after_ any navigation, and send the job to the background to avoid blocking the main
+script. The exception is a forwards navigation, since that requires reading from the database before navigating.
 
-### Q: Is there a way to avoid typing the entire script path?
+### Q: Suggested aliases/functions?
 
 ```bash
 tm() {
-    source path/to/cd_time_machine/main.sh "$@"
+    source path/to/cd_time_machine/main.sh --script_dir=path/to/cd_time_machine --change_dir="$1"
 }
 tmb() {
-    source path/to/cd_time_machine/main.sh --backwards
+    source path/to/cd_time_machine/main.sh --script_dir=path/to/cd_time_machine --backwards
 }
 tmf() {
-    source path/to/cd_time_machine/main.sh --forwards
+    source path/to/cd_time_machine/main.sh --script_dir=path/to/cd_time_machine --forwards
 }
 
 # or
 
 # based on moving around the vim jumplist
-bindkey -s '^O' 'source path/to/cd_time_machine/main.sh --backwards \n'
-bindkey -s '^I' 'source path/to/cd_time_machine/main.sh --forwards \n'
+bindkey -s '^O' 'source path/to/cd_time_machine/main.sh --script_dir=path/to/cd_time_machine --backwards \n'
+bindkey -s '^I' 'source path/to/cd_time_machine/main.sh --script_dir=path/to/cd_time_machine --forwards \n'
 ```
--- main.sh
+++ main.sh
@@ -1,20 +1,11 @@
 # TODO
-# cleanup can't move forwards message
-# look into using z
+# bootstrap script
+# figure out deno_working_dir
 
 total_start=`date +%s.%N`
-# TODO: what to do with this?
-deno_working_dir="$HOME/Desktop/cd_time_machine"
-debug_flag="$2"
 
 red='\033[0;31m'
 no_color='\033[0m'
-
-if [[ "$debug_flag" == "--debug" ]]
-then 
-  # clean logs at the beginning of the script so can view the logs after running the script
-  deno task --cwd="$deno_working_dir" cleanup
-fi
 
 # eg: get_runtime start end label
 # $1: start
@@ -23,64 +14,146 @@ get_runtime() {
   echo "$3 runtime: $( echo "$2 - $1" | bc -l )"
 }
 
-case "$1" in 
-  --forwards)
-    before_nav_pwd=$(pwd)
+# eg: err "bad argument!"
+# $1: message
+err() {
+  echo -e "${red}Unknown option $1${no_color}"
+}
 
-    # run deno command directly to avoid the output from `deno task` i.e. Task push_stack ...
-    if [[ "$2" == "--debug" ]]
-    then 
-      forwards_out=$(deno run --allow-write --allow-env --allow-read --allow-sys --allow-run --unstable-kv "$deno_working_dir/scripts/forwards.ts" --before_nav_pwd="$before_nav_pwd" --debug)
-      echo "forwards_out: $forwards_out"; 
-    else
-      forwards_out=$(deno run --allow-write --allow-env --allow-read --allow-sys --allow-run --unstable-kv "$deno_working_dir/scripts/forwards.ts" --before_nav_pwd="$before_nav_pwd")
-    fi
-    if [[ "$forwards_out" == "__err" ]]
-    then
-      echo -e "${red}Can't move forwards!${no_color}"
+script_dir=""
+change_dir=""
+forwards_flag=0
+backwards_flag=0
+clear_flag=0
+debug_flag=0
+
+for arg in "$@"
+do
+  case "$arg" in 
+    --forwards)
+      forwards_flag=1
+      shift
+      ;;
+    --backwards)
+      backwards_flag=1
+      shift
+      ;;
+    --clear)
+      clear_flag=1
+      shift
+      ;;
+    --debug)
+      debug_flag=1
+      shift
+      ;;
+    --script_dir=*)
+      script_dir=$(echo "$arg" | cut -d'=' -f2)
+      shift
+      ;;
+    --change_dir=*)
+      change_dir=$(echo "$arg" | cut -d'=' -f2)
+      shift
+      ;;
+    --*)
+      err "unknown option $arg"
       return
-    else
-      cd "$forwards_out"
-    fi
-    ;;
-  --backwards)
-    before_nav_pwd=$(pwd)
-    if cd ".."
-      after_nav_pwd=$(pwd)
-      if [[ "$debug_flag" == "--debug" ]]
-      then
-        deno task --cwd="$deno_working_dir" backwards --after_nav_pwd="$after_nav_pwd" --before_nav_pwd="$before_nav_pwd" --debug
-      else 
-        (deno task --cwd="$deno_working_dir" backwards --after_nav_pwd="$after_nav_pwd" --before_nav_pwd="$before_nav_pwd" > /dev/null 2>&1 &)
-      fi
+      ;;
+    *)
+      ;;
+  esac
+done
+
+if [[ "$script_dir" == "" ]] 
+then 
+  err "--script_dir is required!"
+  return
+fi
+
+if [[ "$change_dir" == "" ]]
+then 
+  change_dir_flag=0
+else 
+  change_dir_flag=1
+fi
+
+sum=$((forwards_flag + backwards_flag + clear_flag + change_dir_flag))
+if [[ "$sum" != 1 ]] 
+then 
+  err "only one of --forwards, --backwards, --clear, --change_dir is supported!"
+  return
+fi
+
+if [[ "$debug_flag" == "--debug" ]]
+then 
+  # clean logs at the beginning of the script so can view the logs after running the script
+  deno task --cwd="$script_dir" cleanup
+fi
+
+if [[ "$forwards_flag" == 1 ]]
+then 
+
+  before_nav_pwd=$(pwd)
+
+  # run deno command directly to avoid the output from `deno task` i.e. Task push_stack ...
+  if [[ "$debug_flag" == 1 ]]
+  then 
+    forwards_out=$(deno run --allow-write --allow-env --allow-read --allow-sys --allow-run --unstable-kv "$script_dir/scripts/forwards.ts" --before_nav_pwd="$before_nav_pwd" --debug)
+    echo "forwards_out: $forwards_out"; 
+  else
+    forwards_out=$(deno run --allow-write --allow-env --allow-read --allow-sys --allow-run --unstable-kv "$script_dir/scripts/forwards.ts" --before_nav_pwd="$before_nav_pwd")
+  fi
+  if [[ "$forwards_out" == "__err" ]]
+  then
+    echo -e "${red}Can't move forwards!${no_color}"
+    return
+  else
+    cd "$forwards_out"
+  fi
+
+elif [[ "$backwards_flag" == 1 ]]
+then
+
+  before_nav_pwd=$(pwd)
+  if cd ".."
+    after_nav_pwd=$(pwd)
+    if [[ "$debug_flag" == 1 ]]
     then
-    fi
-    ;;
-  --clear)
-    if [[ "$debug_flag" == "--debug" ]]
-    then
-      deno task --cwd="$deno_working_dir" clear_kv --debug
+      deno task --cwd="$script_dir" backwards --after_nav_pwd="$after_nav_pwd" --before_nav_pwd="$before_nav_pwd" --debug
     else 
-      (deno task --cwd="$deno_working_dir" clear_kv > /dev/null 2>&1 &)
+      (deno task --cwd="$script_dir" backwards --after_nav_pwd="$after_nav_pwd" --before_nav_pwd="$before_nav_pwd" > /dev/null 2>&1 &)
     fi
-    ;;
-  *)
-    before_nav_pwd=$(pwd)
-    if cd "$1"
-    then 
-      after_nav_pwd=$(pwd)
-      if [[ "$debug_flag" == "--debug" ]]
-      then
-        deno task --cwd="$deno_working_dir" push_stack --before_nav_pwd="$before_nav_pwd" --after_nav_pwd="$after_nav_pwd" --debug
-      else 
-        (deno task --cwd="$deno_working_dir" push_stack --before_nav_pwd="$before_nav_pwd" --after_nav_pwd="$after_nav_pwd" > /dev/null 2>&1 &)
-      fi
+  then
+  fi
+
+elif [[ "$clear_flag" == 1 ]]
+then
+
+  if [[ "$debug_flag" == 1 ]]
+  then
+    deno task --cwd="$script_dir" clear_kv --debug
+  else 
+    (deno task --cwd="$script_dir" clear_kv > /dev/null 2>&1 &)
+  fi
+
+elif [[ "$change_dir_flag" == 1 ]]
+then
+
+  before_nav_pwd=$(pwd)
+  if z "$change_dir"
+  then 
+    after_nav_pwd=$(pwd)
+    if [[ "$debug_flag" == 1 ]]
+    then
+      deno task --cwd="$script_dir" push_stack --before_nav_pwd="$before_nav_pwd" --after_nav_pwd="$after_nav_pwd" --debug
+    else 
+      (deno task --cwd="$script_dir" push_stack --before_nav_pwd="$before_nav_pwd" --after_nav_pwd="$after_nav_pwd" > /dev/null 2>&1 &)
     fi
-    ;;
-esac
+  fi
+
+fi
 
 total_end=`date +%s.%N`
-# if [[ "$debug_flag" == "--debug" ]]
-# then 
+if [[ "$debug_flag" == 1 ]]
+then 
   get_runtime "$total_start" "$total_end" "Total"
-# fi
+fi
